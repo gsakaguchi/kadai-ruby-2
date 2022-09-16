a562d02fd10ffa6e6122e71385b829079d194be4
Genta.Sakaguchi <ubuntu@ip-172-31-14-205.ap-northeast-1.compute.internal>
2022-09-16 13:48:12 +0000

    課題提出

---
+++ kadai-fizzbuzz.rb
@@ -0,0 +1,25 @@
+#FizzBuzzの定義
+def fizzbuzz(num)
+
+  #3でも5の倍数でもある場合FizzBuzz
+  if  num%3%5 == 0
+    "FizzBuzz"
+    
+  #5の倍数の場合Buzz
+  elsif num%5 == 0
+    "Buzz"
+
+  #3の倍数の場合Fizz
+  elsif  num%3 == 0
+    "Fizz"
+
+  #それ以外は数値自体を戻す
+  else  
+    num
+  end
+
+end
+
+(1..100).each do |num|
+  puts fizzbuzz(num)
+end
